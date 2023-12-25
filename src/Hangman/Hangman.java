219f54781a6146a35c959920a13fc6c910c2d5f1
OlegYevtushenko519st <o.o.yevtushenko@student.khai.edu>
2023-12-25 23:40:46 +0200

    Hangman 3st stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,20 +1,65 @@
 package Hangman;
-import java.util.Scanner;
+import java.util.*;
 
 public class Hangman {
-    private static final String WORD = "python";
+    private static final List<String> WORDS = Arrays.asList("python", "java", "javascript", "kotlin");
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("HANGMAN");
-        System.out.println("Guess the word:");
 
-        String guess = scanner.nextLine();
+        // Вибір слова
+        int wordIndex = new Random().nextInt(WORDS.size());
+        String word = WORDS.get(wordIndex);
 
-        if (guess.equals(WORD)) {
-            System.out.println("You survived!");
-        } else {
+        // Гра
+        int lives = 8;
+        String guessedLetters = "";
+        while (lives > 0) {
+            // Відображення стану гри
+            System.out.println("");
+            System.out.println(word);
+            System.out.println("Guessed letters: " + guessedLetters);
+
+            // Введення букви
+            System.out.print("Input a letter: ");
+            String letter = scanner.nextLine();
+
+            // Перевірка букви
+            if (letter.length() != 1) {
+                System.out.println("You should input a single letter.");
+                continue;
+            }
+            if (!letter.matches("[a-z]")) {
+                System.out.println("Please enter a lowercase English letter.");
+                continue;
+            }
+
+            // Перевірка, чи буква зустрічається в слові
+            boolean isCorrect = false;
+            for (int i = 0; i < word.length(); i++) {
+                if (letter.equals(word.charAt(i))) {
+                    guessedLetters += letter;
+                    word = word.replace(letter, "-");
+                    isCorrect = true;
+                }
+            }
+
+            // Зміна кількості життів
+            if (!isCorrect) {
+                lives--;
+            }
+
+            // Перевірка на перемогу
+            if (word.equals(guessedLetters)) {
+                System.out.println("You survived!");
+                break;
+            }
+        }
+
+        // Перевірка на поразку
+        if (lives == 0) {
             System.out.println("You lost!");
         }
     }
