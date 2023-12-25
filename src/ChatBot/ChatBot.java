207dd695e385e9fb08b2b964b7575d23a1010a1d
OlegYevtushenko519st <o.o.yevtushenko@student.khai.edu>
2023-12-25 21:54:19 +0200

    ChatBot 1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,10 @@
+package ChatBot;
+public class ChatBot {
+    private static final String BOT_NAME = "MyJavaChatBot";
+    private static final int YEAR_CREATED = 2023;
+    public static void main(String[] args) {
+        // Етап 1
+        System.out.println("Hello! My name is " + BOT_NAME + ".");
+        System.out.println("I was created in " + YEAR_CREATED + ".");
+    }
+}
