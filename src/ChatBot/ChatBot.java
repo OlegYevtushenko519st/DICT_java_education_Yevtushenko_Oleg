36ffb246c06ddd3114c1a9e702e6dffd54b7f284
OlegYevtushenko519st <o.o.yevtushenko@student.khai.edu>
2023-12-25 22:01:14 +0200

    ChatBot 2nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,4 +1,6 @@
 package ChatBot;
+import java.util.Scanner;
+
 public class ChatBot {
     private static final String BOT_NAME = "MyJavaChatBot";
     private static final int YEAR_CREATED = 2023;
@@ -6,5 +8,11 @@ public class ChatBot {
         // Етап 1
         System.out.println("Hello! My name is " + BOT_NAME + ".");
         System.out.println("I was created in " + YEAR_CREATED + ".");
+
+        // Етап 2
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, remind me your name.");
+        String user_name = scanner.nextLine();
+        System.out.println("What a great name you have, " + user_name + "!");
     }
 }
