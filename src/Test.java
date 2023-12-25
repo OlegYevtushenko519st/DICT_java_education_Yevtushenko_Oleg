3ca0fb29a6d46efdd4963c192e1be70f1962f979
OlegYevtushenko519st <o.o.yevtushenko@student.khai.edu>
2023-12-25 20:19:35 +0200

    fix1

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,10 @@ public class Test {
 =======
         System.out.println("education");
         //comment on second string of the func
+<<<<<<< HEAD
 >>>>>>> 74576c0 (added comment in Test.java at the feature branch)
+=======
+        System.out.println("DICT");
+>>>>>>> ec7fd35 (added one more println in Test.java at the feature branch)
     }
 }
