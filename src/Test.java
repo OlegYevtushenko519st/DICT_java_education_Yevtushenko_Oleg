31ba569ced166a66681d9a8baf158e107426119f
OlegYevtushenko519st <o.o.yevtushenko@student.khai.edu>
2023-12-25 20:18:39 +0200

    fix

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,10 @@
 public class Test {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Javaeducation");
+=======
+        System.out.println("education");
+        //comment on second string of the func
+>>>>>>> 74576c0 (added comment in Test.java at the feature branch)
     }
 }
