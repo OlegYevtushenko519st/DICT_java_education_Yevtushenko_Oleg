454a70038722f358061640e912b7734916e5fd98
OlegYevtushenko519st <o.o.yevtushenko@student.khai.edu>
2023-12-26 00:08:34 +0200

    Hangman 5-8st stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,66 +1,96 @@
 package Hangman;
+
 import java.util.*;
 
 public class Hangman {
-    private static final List<String> WORDS = Arrays.asList("python", "java", "javascript", "kotlin");
+
+    private static final String[] WORDS = {"python", "java", "javascript", "kotlin"};
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("HANGMAN");
+        // Меню гри
+        String input;
+        do {
+            System.out.println("HANGMAN");
+            System.out.println("Введіть 'play', щоб почати гру, або 'exit', щоб вийти:");
+            input = scanner.nextLine();
 
-        // Вибір слова
-        int wordIndex = new Random().nextInt(WORDS.size());
-        String word = WORDS.get(wordIndex);
+            if (input.equals("play")) {
+                playGame(scanner);
+            } else if (input.equals("exit")) {
+                System.out.println("Дякуємо за гру!");
+            } else {
+                System.out.println("Неправильний ввід. Спробуйте ще раз.");
+            }
+        } while (!input.equals("exit"));
+    }
 
-        // Гра
-        int lives = 8;
-        String guessedLetters = "";
-        while (lives > 0) {
-            // Відображення стану гри
-            System.out.println("");
-            System.out.println(word.substring(0, 2) + "-");
-            System.out.println("Guessed letters: " + guessedLetters);
+    private static void playGame(Scanner scanner) {
+        // Вибір слова для гри
+        int randomIndex = (int) (Math.random() * WORDS.length);
+        String word = WORDS[randomIndex];
 
-            // Введення букви
-            System.out.print("Input a letter: ");
-            String letter = scanner.nextLine();
+        // Створення масиву для зберігання вгаданих букв
+        char[] guessedLetters = new char[word.length()];
+        Arrays.fill(guessedLetters, '-');
 
-            // Перевірка букви
+        // Счётчик помилок
+        int mistakes = 0;
+
+        // Цикл гри
+        while (mistakes < 8) {
+            // Виведення загаданого слова з вгаданими буквами
+            System.out.println("Загадане слово: " + new String(guessedLetters));
+
+            // Введення букви від гравця
+            System.out.println("Введіть букву:");
+            String letter = scanner.nextLine().toLowerCase();
+
+            // Перевірка введення
             if (letter.length() != 1) {
-                System.out.println("You should input a single letter.");
-                continue;
-            }
-            if (!letter.matches("[a-z]")) {
-                System.out.println("Please enter a lowercase English letter.");
+                System.out.println("Введіть одну букву.");
                 continue;
             }
 
-            // Перевірка, чи буква зустрічається в слові
-            boolean isCorrect = false;
-            for (int i = 0; i < word.length(); i++) {
-                if (letter.equals(word.charAt(i))) {
-                    guessedLetters += letter;
-                    word = word.replace(letter, "-");
-                    isCorrect = true;
+            // Перевірка, чи буква вже вгадувалася
+            if (contains(guessedLetters, letter.charAt(0))) {
+                System.out.println("Цю букву ви вже вгадували.");
+                continue;
+            }
+
+            // Перевірка, чи буква є в загаданому слові
+            if (word.indexOf(letter.charAt(0)) == -1) {
+                mistakes++;
+                System.out.println("Ця буква не входить до слова.");
+            } else {
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.charAt(i) == letter.charAt(0)) {
+                        guessedLetters[i] = letter.charAt(0);
+                    }
                 }
             }
 
-            // Зміна кількості життів
-            if (!isCorrect) {
-                lives--;
-            }
-
-            // Перевірка на перемогу
-            if (word.equals(guessedLetters)) {
-                System.out.println("You survived!");
+            // Перевірка, чи гравець вгадав слово
+            if (Arrays.equals(guessedLetters, word.toCharArray())) {
+                System.out.println("Ви вгадали слово! Ви вижили!");
                 break;
             }
         }
 
-        // Перевірка на поразку
-        if (lives == 0) {
-            System.out.println("You lost!");
+        // Гра закінчилася
+        if (mistakes == 8) {
+            System.out.println("Ви програли!");
         }
+    }
+
+    private static boolean contains(char[] array, char c) {
+        for (char letter : array) {
+            if (letter == c) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
