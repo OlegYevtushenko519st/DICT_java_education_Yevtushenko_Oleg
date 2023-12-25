55fb0aef94d1f3201d90e079da15b2d9b9a08581
OlegYevtushenko519st <o.o.yevtushenko@student.khai.edu>
2023-12-25 22:10:54 +0200

    ChatBot 3rd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -14,5 +14,14 @@ public class ChatBot {
         System.out.println("Please, remind me your name.");
         String user_name = scanner.nextLine();
         System.out.println("What a great name you have, " + user_name + "!");
+
+        // Етап 3
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+        int user_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.println("Your age is " + user_age + "; that's a good time to start programming!");
     }
 }
