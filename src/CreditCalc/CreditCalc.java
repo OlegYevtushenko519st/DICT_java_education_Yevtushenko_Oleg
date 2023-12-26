4c1bfe213851a5495cfd2f5a0217ec7ddea3510e
OlegYevtushenko519st <o.o.yevtushenko@student.khai.edu>
2023-12-26 19:52:28 +0200

    CreditCalc 1st stage

---
+++ src/CreditCalc/CreditCalc.java
@@ -0,0 +1,21 @@
+package CreditCalc;
+
+public class CreditCalc {
+    public static void main(String[] args) {
+
+        // Виводимо початковий розмір кредиту
+        System.out.println("Loan principal: 1000");
+
+        // Виводимо суму погашення за перший місяць
+        System.out.println("Month 1: repaid 250");
+
+        // Виводимо суму погашення за другий місяць
+        System.out.println("Month 2: repaid 250");
+
+        // Виводимо суму погашення за третій місяць
+        System.out.println("Month 3: repaid 500");
+
+        // Виводимо повідомлення про те, що кредит погашений
+        System.out.println("The loan has been repaid!");
+    }
+}
